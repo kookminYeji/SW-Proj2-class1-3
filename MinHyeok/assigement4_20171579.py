177a6bcaa7682b2e45f61be47b718a9ce7d5ce88
MinHyeoks <koomh0319@gmail.com>
2017-09-26 10:45:45 +0900

    update assigment

---
+++ MinHyeok/assigement4_20171579.py
@@ -0,0 +1,36 @@
+num = int(input())
+knum = int(input())
+
+def factorialRecursive(n):
+    return 1 if n == 1 else n * factorialRecursive(n-1)
+
+def factorial_wf(n):
+    i = 0
+    _factorial = 1
+    while ( i < n):
+        _factorial = _factorial * ( i + 1)
+        i+=1
+    return _factorial
+
+
+def Combination(n,k):
+    if k == 0 or n == k:
+        return 1
+    elif n < k:
+        print(" n < k cannot do it")
+    else:
+        return int(Combination(n-1,k-1)+Combination(n-1,k))
+
+def Combination_wf(n,k):
+    return int(factorial_wf(n) / (factorial_wf(k) * factorial_wf(n-k)))
+
+
+print(factorialRecursive(num))
+print(factorial_wf(num))
+
+print(Combination_wf(num,knum))
+print(Combination(num,knum))
+
+
+
+
