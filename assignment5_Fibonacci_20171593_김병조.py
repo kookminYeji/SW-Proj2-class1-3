5961be4448763e0b035539948017c3f481413024
byeongjo <gee05053@kookmin.ac.kr>
2017-10-10 11:08:16 +0900

    code review

--- assignment5_Fibonacci_20171593_김병조.py
+++ assignment5_Fibonacci_20171593_김병조.py
@@ -59,3 +59,34 @@ print("정해준 n값 = %d, 결과값 = %d, time = %.6f" %(n, number_list[len(nu
 start2 = time.time()
 print("정해준 n값 = %d, 결과값 = %d, time = %.6f" %(n, fibo(n), time.time()-start2))
 """
+"""
+import time
+
+def fibo(n):
+    if n <= 1:
+        return n
+    return fibo(n-1) + fibo(n-2)
+
+def iterfibo(n):
+    fibList = [0, 1]
+    i = 2
+    while True:
+        if n < i:
+            break
+        fibList.append(fibList[i-2] + fibList[i-1])
+        i += 1
+    return fibList.pop()
+
+while True:
+    nbr = int(input("Enter a number: "))
+    if nbr == -1:
+        break
+    ts = time.time()
+    fibonumber = iterfibo(nbr)
+    ts = time.time() - ts
+    print("IterFibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+    ts = time.time()
+    fibonumber = fibo(nbr)
+    ts = time.time() - ts
+    print("Fibo(%d) = %d, time %.6f" %(nbr, fibonumber, ts))
+"""
